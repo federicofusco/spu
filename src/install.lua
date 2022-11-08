7ddc6b66a6ade53dea7d5bb8fa6a9704cb73d12f
Federico Fusco <federicofusco0@gmail.com>
2022-11-09 00:06:38 +0100

    Updated installer

--- src/install.lua
+++ src/install.lua
@@ -1,9 +1,11 @@
 -- This is a file which should be called to init the SCU system
 
 -- The lib URLs
-local libURL = "https://raw.githubusercontent.com/federicofusco/spu/main/src/core"
-local serviceURL = "https://raw.githubusercontent.com/federicofusco/spu/main/src/util/"
-local configURL = "https://raw.githubusercontent.com/federicofusco/spu/main/src/config"
+local baseURL = "https://raw.githubusercontent.com/federicofusco/spu/main/src"
+local libURL = baseURL .. "/core"
+local serviceURL = baseURL .. "/services"
+local configURL = baseURL .. "/config"
+local cliURL = baseURL .. "/cli"
 
 -- Creates the SCU directory
 local directory = "/scu"
@@ -21,3 +23,13 @@ fs.open ( directory .. "/services/logs/harvestd.log", "w" ).close ()
 
 -- [[ Creates default config files ]]
 shell.run ( "wget", configURL .. "/harvestd.toml", directory .. "/config/harvestd.toml" )
+
+-- [[ Sets up the CLI ]]
+shell.run ( "wget", cliURL .. "/main.lua", directory .. "/cli/main.lua" )
+shell.run ( "wget", cliURL .. "/stdout.lua", directory .. "/cli/stdout.lua" )
+shell.run ( "wget", cliURL .. "/command.lua", directory .. "/cli/command.lua" )
+shell.run ( "wget", cliURL .. "/help/start.lua", directory .. "/cli/help/start.lua" )
+shell.run ( "wget", cliURL .. "/help/nil.lua", directory .. "/cli/help/nil.lua" )
+
+-- [[ Sets up aliases ]]
+shell.setAlias ( "scu", "/scu/cli/main" )
